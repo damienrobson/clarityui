6278756da20ffd65d0b283def5f4aa4223297c04
Damien Robson <damien.p.robson@gmail.com>
2025-09-10 22:09:30 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import { extname, relative, resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "vite-plugin-dts";
+import { glob } from "glob";
+import { fileURLToPath } from "url";
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/**/*.{ts,tsx}", {
+            ignore: ["lib/**/*.d.ts", "lib/**/*.stories.tsx"],
+          })
+          .map((file) => [
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
+});
